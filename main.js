7da37cb97f43374da443c0bb7d7eadfe7af3b464
Raquel Botelho <raquel.botelhoof@gmail.com>
2025-08-24 13:50:06 -0300

    add cont

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,7 +8,7 @@ let contador = 0;
 
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
