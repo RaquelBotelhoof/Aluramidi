1a9b90848bd19522d008ee2771fe502b860299a2
Raquel Botelho <raquel.botelhoof@gmail.com>
2025-08-23 21:36:13 -0300

    add function

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('Olá Mundo');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
